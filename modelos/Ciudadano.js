28635cfc41f5e27fd1d919ae9e078b6bb6d5a84a
mykeprojects <mykeprojects@gmail.com>
2026-02-27 11:09:21 -0500

    Se realizan comentarios a la clase ciudadano

--- modelos/Ciudadano.js
+++ modelos/Ciudadano.js
@@ -1,4 +1,5 @@
 class Ciudadano{
+    //Se implementa el constructor de ciudadano, especificando que si no se mandan ciertos atributos se asuman null.
     constructor(id, felicidad, vivienda, empleo, consumoCiudadano){
         this._id = id;
         this._felicidad = felicidad || null;
@@ -7,10 +8,12 @@ class Ciudadano{
         this._consumoCiudadano = consumoCiudadano;
     }
 
+    //Se edita el consumo del ciudadano para un único recurso, según la necesidad del sistema
     editarConsumoCiudadano(recurso, valor){
         this._consumoCiudadano[recurso] = valor;
     }
 
+    //Se calcula la felicidad individual del ciudadano, según el paramétro de felicidad general dada por las edificaciones
     calcularFelicidad(felicidadGeneral){
         this._felicidad = felicidadGeneral
         this._felicidad += this._vivienda ? 20 : -20
