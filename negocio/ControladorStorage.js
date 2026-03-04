b4376649aa6d7292128d8f8bba324c76d259d771
mykeprojects <mykeprojects@gmail.com>
2026-03-03 20:14:46 -0500

    Se comenta la funcionalidad de cada uno de los métodos en el ControladorStorage

--- negocio/ControladorStorage.js
+++ negocio/ControladorStorage.js
@@ -11,11 +11,13 @@ class ControladorStorage{
                 edificio: JSON.parse(JSON.stringify(obj))
             };
         });
-        
+        // Asignamos la lista serializada a los edificios de la copia de ciudad
         ciudadCopia.terreno.edificios = edificios;
         CiudadStorage.guardar(ciudadCopia);
     }
 
+    //Se genera un objeto que contiene cada uno de los tipos de edificios
+    //Se usa para la reconstrucción de la ciudad al cargarla
     static TipoClases = {
         Apartamento,
         Casa,
@@ -32,11 +34,14 @@ class ControladorStorage{
         Via
     }
 
+    //Método encargado de la reconstrucción de la ciudad.
     static cargarCiudad(){
+        //Llamaddo de métodos
         const datos = JSON.parse(CiudadStorage.cargar());
         let ciudadanos = this.obtenerCiudadanos(datos)
         let edificios = this.obtenerEdificios(datos);
         let terreno = new Terreno(datos.terreno.vias, edificios);
+        //Construcción de la ciudad
         let ciudad = new Ciudad(
             datos.nombre,
             datos.alcalde,
@@ -47,10 +52,12 @@ class ControladorStorage{
             ciudadanos,
             datos.estadoRecursos
         )
+        //Se realiza la rehidratación de los ciudadanos
         this.rehidratarCiudadanos(ciudad);
         return ciudad;
     }
 
+    //Método para cargar los ciudadanos desde el local storage
     static obtenerCiudadanos(datos){
         let ciudadanos = datos.ciudadanos.map(c => 
             Ciudadano.fromData(c)
@@ -58,7 +65,9 @@ class ControladorStorage{
         return ciudadanos;
     }
 
+    //Método para la reconstrucción de los edificios con su respectivo tipo(clase)
     static obtenerEdificios(datos){
+        //En esta parte se usa el objeto TipoClases para saber a que clase hace referencia
         let edificios = datos.terreno.edificios.map(e => {
             const claseref = ControladorStorage.TipoClases[e.tipo];
             return claseref.fromData(e.edificio);
@@ -66,9 +75,11 @@ class ControladorStorage{
         return edificios;
     }
 
+    //Se encarga de guardar los ciudadanos de ciudad en cada uno de sus respectivos edificios
     static rehidratarCiudadanos(ciudad){
+        //Se genera un mapa de ciudadanos con pares clave, valor
         const MapaCiudadanos = new Map(ciudad.ciudadanos.map(c => [c.id, c]))
-
+        //Por cada edificio lee la lista de ciudadanos y compara el id para volver a asignar las instancias
         ciudad.terreno.edificios.forEach(e => 
             e.ciudadanos = e.ciudadanos.map(id => {
                 const objetoCiudadano = MapaCiudadanos.get(id.id);
--- negocio/Entorno_Sintetico.js
+++ negocio/Entorno_Sintetico.js
@@ -20,13 +20,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const via2 = new Via({fila: 2, columna: 2});
     const via3 = new Via({fila: 2, columna: 3});
     const casa = new Casa({fila: 3, columna: 2});
-    const granja = new Tienda({fila: 1, columna: 1});
+    const tienda = new Tienda({fila: 1, columna: 1});
 
     terreno.crearInfraestructura(2, 1, via1);
     terreno.crearInfraestructura(2, 2, via2);
     terreno.crearInfraestructura(2, 3, via3);
     terreno.crearInfraestructura(3, 2, casa);
-    terreno.crearInfraestructura(1, 1, granja);
+    terreno.crearInfraestructura(1, 1, tienda);
     ciudad.asignarVivienda(ciudadano1);
     ciudad.asignarEmpleo(ciudadano1);
     ciudad.asignarVivienda(ciudadano2);
@@ -34,6 +34,6 @@ document.addEventListener("DOMContentLoaded", function () {
     ControladorStorage.guardarCiudad(ciudad);
     ciudadnueva = ControladorStorage.cargarCiudad();
     console.log(ciudadnueva)
-    console.log(ciudadnueva.terreno.edificios[4].ciudadanos[0] instanceof Ciudadano)
+    console.log(ciudadnueva.terreno.edificios[4] instanceof Tienda)
     
 })
