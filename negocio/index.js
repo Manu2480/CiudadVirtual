6527cc278c13f0c5c60499b8c8091a0eb4914d4a
Manu2480 <manuvallejot@gmail.com>
2026-03-05 14:00:13 -0500

    media actualido con img para botones de crear y cargar

---
+++ negocio/index.js
@@ -0,0 +1,9 @@
+const logo = document.getElementById("logo");
+const botones = document.getElementById("botones");
+
+logo.addEventListener("click", () => {
+
+    logo.style.display = "none";
+    botones.style.display = "flex";
+
+});
